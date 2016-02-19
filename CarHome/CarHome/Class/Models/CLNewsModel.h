a1346dee10e7fe770aee0264a544ffb2bb537dd7
json snow <jsonsnow@163.com>
2016-02-19 16:28:52 +0800

    create a NewsModel to storage new messages

---
+++ CarHome/CarHome/Class/Models/CLNewsModel.h
@@ -0,0 +1,13 @@
+//
+//  CLNewsModel.h
+//  CarHome
+//
+//  Created by Aspmcll on 16/2/19.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLNewsModel : NSObject
+
+@end
---
+++ CarHome/CarHome/Class/Models/CLNewsModel.m
@@ -0,0 +1,13 @@
+//
+//  CLNewsModel.m
+//  CarHome
+//
+//  Created by Aspmcll on 16/2/19.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+#import "CLNewsModel.h"
+
+@implementation CLNewsModel
+
+@end
