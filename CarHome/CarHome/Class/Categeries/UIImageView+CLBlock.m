2df81b56575090b132412337c7c694fc2960e80a
json snow <jsonsnow@163.com>
2016-02-20 20:05:08 +0800

    create a catereger for uiimageView use runtime this function add a blcokprooery

---
+++ CarHome/CarHome/Class/Categeries/UIImageView+CLBlock.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+CLBlock.m
+//  CarHome
+//
+//  Created by Aspmcll on 16/2/20.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+#import "UIImageView+CLBlock.h"
+
+@implementation UIImageView (CLBlock)
+
+@end
---
+++ CarHome/CarHome/Class/Controllers/CLDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLDetailViewController.h
+//  CarHome
+//
+//  Created by Aspmcll on 16/2/20.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLDetailViewController : UIViewController
+
+@end
